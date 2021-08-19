602b89f8f7277ef1c3322dc11f78401817906159
Gianni Zamora <giannizcool@gmail.com>
2021-08-18 21:43:55 -0700

    setup firstname and lastname auth, now starting implemnting SPA to laravel

---
+++ resources/js/store/index.js
@@ -0,0 +1,13 @@
+import { values } from 'lodash';
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import currentUser from './modules/currentUser';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {
+        currentUser
+    }
+});
